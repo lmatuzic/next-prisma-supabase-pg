5e1f4b6d038f43c6f57d5f4210a10e11538b8be5
lmatuzic <luka.matuzic1@gmail.com>
2023-08-12 19:37:38 +0200

    added navbar and mobile nav

---
+++ app/components/navigation/MobileNavigation.tsx
@@ -0,0 +1,19 @@
+import { Menu } from 'lucide-react';
+import { Sheet, SheetContent, SheetDescription, SheetTrigger } from '../shadcn/sheet';
+
+export default function MobileNavigation() {
+	return (
+		<Sheet>
+			<SheetTrigger>
+				<Menu />
+			</SheetTrigger>
+
+			<SheetContent className='pt-12'>
+				<SheetDescription>
+					This action cannot be undone. This will permanently delete your account and remove your
+					data from our servers.
+				</SheetDescription>
+			</SheetContent>
+		</Sheet>
+	);
+}
---
+++ app/components/navigation/Navigation.tsx
@@ -0,0 +1,34 @@
+// import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import MobileNavigation from './MobileNavigation';
+import ThemeToggleButton from '../button/ThemeToggleButton';
+
+export default function Navigation() {
+	return (
+		<nav className='w-full px-4 py-2 mb-8 border-b'>
+			<div className='container flex items-center justify-between p-0'>
+				<Link href={'/'} className='hidden sm:flex'>
+					Logo
+				</Link>
+
+				<ul className='flex justify-between w-full sm:w-auto sm:items-center'>
+					<li className='hidden mr-6 sm:flex'>
+						<Link href={'/interests'}>Interests</Link>
+					</li>
+					{/* 
+					<li className='hidden mr-6 sm:flex'>
+						<UserButton afterSignOutUrl='/' />
+					</li> */}
+
+					<li>
+						<ThemeToggleButton />
+					</li>
+
+					<li className='flex sm:hidden'>
+						<MobileNavigation />
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
